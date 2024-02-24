8d6a208cbaceef3206ad1ce71654fd8482cec2e8
OverlordsIII <shrihunreddy@outlook.com>
2024-02-24 15:20:24 -0800

    initial commit

---
+++ src/client/java/io/github/overlordsiii/ElytraOverhaulClient.java
@@ -0,0 +1,13 @@
+package io.github.overlordsiii;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElytraOverhaulClient implements ClientModInitializer {
+	/**
+	 * Runs the mod initializer on the client environment.
+	 */
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/io/github/overlordsiii/ElytraOverhaul.java
@@ -0,0 +1,13 @@
+package io.github.overlordsiii;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ElytraOverhaul implements ModInitializer {
+	/**
+	 * Runs the mod initializer.
+	 */
+	@Override
+	public void onInitialize() {
+
+	}
+}
